f84f0ee31c4659c4d35cc3e2ec067e939d7b22c0
Roman Stefanidi <stefanidi@gmail.com>
2015-12-10 02:11:59 +1100

    full revert?

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-# from cgi import parse_qs, escape
-
 def wsgi_app(environ, start_response):
-    # parameters = parse_qs(environ.get('QUERY_STRING', ''))
-    # if 'subject' in parameters:
-    #     subject = escape(parameters['subject'][0])
-    # else:
-    #     subject = 'World'
-    # subject = 'World'
-    # start_response('200 OK', [('Content-Type', 'text/html')])
-    # return ['''Hello %(subject)s Hello %(subject)s!''' % {'subject': subject}]
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
