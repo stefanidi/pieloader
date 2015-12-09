7060ad75d3e79eae8df70b5a24091893a18d1765
Roman Stefanidi <stefanidi@gmail.com>
2015-12-10 02:27:08 +1100

    added imli and key

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ from cgi import parse_qs, escape
 
 def wsgi_app(environ, start_response):
     parameters = parse_qs(environ.get('QUERY_STRING', ''))
-    if 'subject' in parameters:
-        subject = escape(parameters['subject'][0])
-    else:
-        subject = 'World'
+    imei = escape(parameters['imei'][0])
+    lkey = escape(parameters['lkey'][0])
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    response_body = 'Hello World ' + subject
+    response_body = 'Imei: ' + imei + ' lkey: ' + lkey
     yield response_body.encode()
 
 if __name__ == '__main__':
