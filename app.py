ff6949bb1d5e06297eef5b87085be2db7578eaac
Roman Stefanidi <stefanidi@gmail.com>
2015-12-10 02:03:58 +1100

    testing

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from cgi import parse_qs, escape
+# from cgi import parse_qs, escape
 
 def wsgi_app(environ, start_response):
-    parameters = parse_qs(environ.get('QUERY_STRING', ''))
-    if 'subject' in parameters:
-        subject = escape(parameters['subject'][0])
-    else:
-        subject = 'World'
+    # parameters = parse_qs(environ.get('QUERY_STRING', ''))
+    # if 'subject' in parameters:
+    #     subject = escape(parameters['subject'][0])
+    # else:
+    #     subject = 'World'
+    subject = 'World'
     start_response('200 OK', [('Content-Type', 'text/html')])
     return ['''Hello %(subject)s Hello %(subject)s!''' % {'subject': subject}]
 
