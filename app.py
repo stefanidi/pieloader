4116d651526da05745235f56bb2c331797548e0a
Roman Stefanidi <stefanidi@gmail.com>
2015-12-10 02:17:44 +1100

    added parameter parsing

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from cgi import parse_qs, escape
 
-def wsgi_app(environ, start_response):
+def wsgi_app(environ, start_response)
+    parameters = parse_qs(environ.get('QUERY_STRING', ''))
+    if 'subject' in parameters:
+        subject = escape(parameters['subject'][0])
+    else:
+        subject = 'World'
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    response_body = 'Hello World'
+    response_body = 'Hello World ' + subject
     yield response_body.encode()
 
 if __name__ == '__main__':
